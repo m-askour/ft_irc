3e8e57186c2d031715a7d3cab0b842f92f703cc7
MOHAMED ASKOUR <116392756+m-askour@users.noreply.github.com>
2026-05-06 21:39:42 +0100

    Enhance client.hpp with comments on client and operators
    Added detailed comments explaining client functionality and operator roles in the IRC network.

--- ft_irc/client/client.hpp
+++ ft_irc/client/client.hpp
@@ -2,6 +2,36 @@
 #define CLIENT_HPP
 
 #include "../server/server.hpp"
+// ##1.2 Clients
+
+//    A client is anything connecting to a server that is not another
+//    server.  Each client is distinguished from other clients by a unique
+//    nickname having a maximum length of nine (9) characters.  See the
+//    protocol grammar rules for what may and may not be used in a
+//    nickname.  In addition to the nickname, all servers must have the
+//    following information about all clients: the real name of the host
+//    that the client is running on, the username of the client on that
+//    host, and the server to which the client is connected.
+
+// 1.2.1 Operators
+
+//    To allow a reasonable amount of order to be kept within the IRC
+//    network, a special class of clients (operators) is allowed to perform
+//    general maintenance functions on the network.  Although the powers
+//    granted to an operator can be considered as 'dangerous', they are
+//    nonetheless required.  Operators should be able to perform basic
+//    network tasks such as disconnecting and reconnecting servers as
+//    needed to prevent long-term use of bad network routing.  In
+//    recognition of this need, the protocol discussed herein provides for
+//    operators only to be able to perform such functions.  See sections
+//    4.1.7 (SQUIT) and 4.3.5 (CONNECT).
+
+//    A more controversial power of operators is the ability  to  remove  a
+//    user  from  the connected network by 'force', i.e. operators are able
+//    to close the connection between any client and server.   The
+//    justification for  this  is delicate since its abuse is both
+//    destructive and annoying.  For further details on this type of
+//    action, see section 4.6.1 (KILL).
 
 class client : public server
 {
